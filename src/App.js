6fe7fafd64667c7354df68786393968cb0241d4a
Ramit Kumar Gupta <54888662+ramitnitp@users.noreply.github.com>
2023-01-27 15:14:24 +0000

    "start"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+           Codespaces  Bharat Bhushan
+          {/* <span className="heart">♥️</span> React */}
         </p>
         <p className="small">
           Edit <code>src/App.js</code> and save to reload.
